c24b50acc5058131d1d8eb14b9d1ad6724160b3c
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-09-11 04:16:46 -0300

    chore: move cache and update packages

--- packages/ui/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import baseConfig from "@repo/tailwind-config/web"
-import type { Config } from "tailwindcss"
-
-export default {
-	content: ["./src/**/*.tsx"],
-	presets: [baseConfig]
-} satisfies Config
