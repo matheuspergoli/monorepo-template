5539f268e70e02ba242289c7a5188251662ef085
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2026-01-25 02:06:15 -0300

    teste

--- apps/website/src/routes/index.tsx
+++ apps/website/src/routes/index.tsx
@@ -1,13 +1,19 @@
+import { Button } from "@repo/ui/components/button"
 import { createFileRoute } from "@tanstack/react-router"
+import { useAuth } from "@/libs/auth"
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent
 })
 
 function RouteComponent() {
+	const { login } = useAuth()
+
 	return (
 		<main className="flex h-screen w-screen items-center justify-center">
 			<h1>Hello World!</h1>
+
+			<Button onClick={async () => await login()}>Login</Button>
 		</main>
 	)
 }
