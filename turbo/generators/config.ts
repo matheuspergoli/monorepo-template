695300df3ef39a938ff6efcaed8841d7ba5c4a63
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2026-01-30 03:01:42 -0300

    modify turbo generators

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -9,19 +9,13 @@ interface PackageJson {
 }
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-	plop.setGenerator("init", {
-		description: "Generate a new package for the Monorepo",
+	plop.setGenerator("app", {
+		description: "Generate a new app for the Monorepo",
 		prompts: [
 			{
 				type: "input",
 				name: "name",
-				message: "What is the name of the package? (You can skip the `@repo/` prefix)"
-			},
-			{
-				type: "input",
-				name: "workspace",
-				message: "Which workspace should the package be created? (e.g. packages, apps)",
-				default: "packages"
+				message: "What is the name of the app? (You can skip the `@repo/` prefix)"
 			},
 			{
 				type: "input",
@@ -40,22 +34,91 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			},
 			{
 				type: "add",
-				path: "{{ workspace }}/{{ name }}/package.json",
-				templateFile: "templates/package.json.hbs"
+				path: "apps/{{ name }}/package.json",
+				templateFile: "templates/app/package.json.hbs"
 			},
 			{
 				type: "add",
-				path: "{{ workspace }}/{{ name }}/tsconfig.json",
-				templateFile: "templates/tsconfig.json.hbs"
+				path: "apps/{{ name }}/tsconfig.json",
+				templateFile: "templates/app/tsconfig.json.hbs"
 			},
 			{
 				type: "add",
-				path: "{{ workspace }}/{{ name }}/src/index.ts",
+				path: "apps/{{ name }}/src/index.ts",
 				template: "export const name = '{{ name }}';"
 			},
 			{
 				type: "modify",
-				path: "{{ workspace }}/{{ name }}/package.json",
+				path: "apps/{{ name }}/package.json",
+				async transform(content, answers) {
+					if ("deps" in answers && typeof answers.deps === "string") {
+						const pkg = JSON.parse(content) as PackageJson
+						for (const dep of answers.deps.split(" ").filter(Boolean)) {
+							const version = await fetch(
+								`https://registry.npmjs.org/-/package/${dep}/dist-tags`
+							)
+								.then((res) => res.json())
+								.then((json) => json.latest)
+							if (!pkg.dependencies) pkg.dependencies = {}
+							pkg.dependencies[dep] = `^${version}`
+						}
+						return JSON.stringify(pkg, null, 2)
+					}
+					return content
+				}
+			},
+			(answers) => {
+				if ("name" in answers && typeof answers.name === "string") {
+					execSync("pnpm i", { stdio: "inherit" })
+					execSync("pnpm format")
+					return "App scaffolded"
+				}
+				return "App not scaffolded"
+			}
+		]
+	})
+
+	plop.setGenerator("package", {
+		description: "Generate a new package for the Monorepo",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "What is the name of the package? (You can skip the `@repo/` prefix)"
+			},
+			{
+				type: "input",
+				name: "deps",
+				message: "Enter a space separated list of dependencies you would like to install"
+			}
+		],
+		actions: [
+			(answers) => {
+				if ("name" in answers && typeof answers.name === "string") {
+					if (answers.name.startsWith("@repo/")) {
+						answers.name = answers.name.replace("@repo/", "")
+					}
+				}
+				return "Config sanitized"
+			},
+			{
+				type: "add",
+				path: "packages/{{ name }}/package.json",
+				templateFile: "templates/package/package.json.hbs"
+			},
+			{
+				type: "add",
+				path: "packages/{{ name }}/tsconfig.json",
+				templateFile: "templates/package/tsconfig.json.hbs"
+			},
+			{
+				type: "add",
+				path: "packages/{{ name }}/src/index.ts",
+				template: "export const name = '{{ name }}';"
+			},
+			{
+				type: "modify",
+				path: "packages/{{ name }}/package.json",
 				async transform(content, answers) {
 					if ("deps" in answers && typeof answers.deps === "string") {
 						const pkg = JSON.parse(content) as PackageJson
